e38c37aaccb00d34fe0cde16fa9dccdcb28a5894
londaploetz <lploetz@hotmail.com>
2023-01-05 23:06:10 -0600

    adding files

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Todo } from './components'
+
+const App = () => (
+  <div className="App">
+ <div className='background_container'>
+    <Todo />
+  </div>
+</div>
+);
+
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+
+const app = express();
+
+
+app.listen(3001, ()=>{
+    console.log(`Server is running on 3001`)
+})
