a08e7c8f7e2c14d693b3b958c5bc747be57e6dbb
londaploetz <lploetz@hotmail.com>
2023-04-20 15:37:49 -0500

    deleted usecase

--- index.js
+++ index.js
@@ -21,13 +21,6 @@ app.use(express.json());
 app.use(cors());
 
 
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
-
-
 // declare react files in build as static
 app.use(express.static(path.join(__dirname, "build")));
 
